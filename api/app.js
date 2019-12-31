f056b8d4d84e51bbd337fdef22ec7dc248c45789
Sebastian Echazú <sebastianechazu@outlook.com>
2019-12-31 09:57:58 -0300

    configuracion de cabeceras finalizado

--- api/app.js
+++ api/app.js
@@ -21,7 +21,15 @@ app.use(bodyParser.json());
 //-----------------------------------------------------------------------------------------
 //cors
 //-----------------------------------------------------------------------------------------
-
+// configurar cabeceras http
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+ 
+    next();
+});
 //-----------------------------------------------------------------------------------------
 //CREACION DE LAS RUTAS
 //-----------------------------------------------------------------------------------------
