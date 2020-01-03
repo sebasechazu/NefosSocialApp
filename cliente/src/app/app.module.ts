cf5fe2f9ef27763f6e5f5e35169c52a2406f029c
Sebastian Echazú <sebastianechazu@outlook.com>
2020-01-03 20:53:25 -0300

    compnentes creados register y login

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//-------------------------------------------------------------------------------------------------
+//COMPONENTES 
+//-------------------------------------------------------------------------------------------------
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ cliente/src/app/components/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public title: string;
+
+  constructor() { 
+    this.title = "identificate";
+  }
+
+  ngOnInit() {
+    console.log('Componente de login cargando...'); 
+  }
+
+}
