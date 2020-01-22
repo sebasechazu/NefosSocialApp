16c9daaa8c2d4f4557ccf4780eaf75e9ba2b436b
Sebastian Echazú <sebastianechazu@outlook.com>
2020-01-21 22:58:50 -0300

    login componente

--- cliente/src/app/components/login/login.component.ts
+++ cliente/src/app/components/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
 
 @Component({
   selector: 'app-login',
