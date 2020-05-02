b32f38ff90d3405030cfe80a5f86bbde1b6b5644
Sebastian Echazu <sebastianechazu@outlook.com>
2020-05-02 13:11:41 -0300

    comentarios

--- api/app.js
+++ api/app.js
@@ -1,25 +1,24 @@
 'use strict'
 //-----------------------------------------------------------------------------------------
-//ARCHIVO DE CONFIGURACION DE EXPRESS
+// ARCHIVO DE CONFIGURACION DE EXPRESS
 //-----------------------------------------------------------------------------------------
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var app = express();
 //-----------------------------------------------------------------------------------------
-//INSTANCIACION DE LA RUTAS
+// INSTANCIACION DE LA RUTAS
 //-----------------------------------------------------------------------------------------
 var user_routes = require('./routes/user');
 var follow_routes = require('./routes/follow');
 var publication_routes = require('./routes/Publication');
 var message_routes = require('./routes/message');
 //-----------------------------------------------------------------------------------------
-//MIDDLEWARES
+// MIDDLEWARES
 //-----------------------------------------------------------------------------------------
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //-----------------------------------------------------------------------------------------
-//cors
+// CORS
 //-----------------------------------------------------------------------------------------
 // configurar cabeceras http
 app.use((req, res, next) => {
@@ -27,15 +26,16 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-
     next();
 });
 //-----------------------------------------------------------------------------------------
-//CREACION DE LAS RUTAS
+// CREACION DE LAS RUTAS
 //-----------------------------------------------------------------------------------------
 app.use('/api', user_routes);
 app.use('/api', follow_routes);
 app.use('/api', publication_routes);
 app.use('/api', message_routes);
-
+//-----------------------------------------------------------------------------------------
+// EXPORTAMOS FUERA DEL ARCHIVO
+//-----------------------------------------------------------------------------------------
 module.exports = app;
--- api/index.js
+++ api/index.js
@@ -1,23 +1,22 @@
 'use strict'
 //-----------------------------------------------------------------------------------------
-//ARCHIVO DE CRACION DEL API
+//ARCHIVO DE CREACION DEL API
 //-----------------------------------------------------------------------------------------
+// instanciamos moongoose para administrar mongoDb
 var mongoose = require('mongoose');
-
-var app= require('./app');
-
+// instanciamos nuestro archivo de aplicacion
+var app = require('./app');
+// variable de puerto de conexion de nuestro api
 var port = 3800;
-
+// iniciamos mongoose
 mongoose.Promise = global.Promise;
-
-//conexion a la base de datos
-mongoose.connect('mongodb+srv://NefosAppUser:dana2002@cluster0-rxn0l.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true,useUnifiedTopology: true})
-    .then(()=> {
-        console.log("la conexion a la base de datos NEFOSSOCIALAPP se realizo con exito");
-
-        // crear servidor
-
-        app.listen(port,()=> {
+//conexion a la base de datos alojada en mongoDb atlas
+mongoose.connect('mongodb+srv://NefosAppUser:dana2002@cluster0-rxn0l.mongodb.net/test?retryWrites=true&w=majority'
+    , { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("la conexion a la base de datos Api de NefosSocial se se realizo con exito");
+        // abrimos el puerto
+        app.listen(port, () => {
             console.log("servidor corriendo http://localhost:3800");
         })
     })
