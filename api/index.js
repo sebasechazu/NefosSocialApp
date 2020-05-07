6bb3eb6c730f22b170fbe453f27469817182258e
Sebastian Echazú <sebastianechazuedu@gmail.com>
2020-05-07 12:00:06 -0300

    comentarios de index.js

--- api/index.js
+++ api/index.js
@@ -1,23 +1,27 @@
 'use strict'
-//-----------------------------------------------------------------------------------------
-//ARCHIVO DE CREACION DEL API
-//-----------------------------------------------------------------------------------------
-// instanciamos moongoose para administrar mongoDb
+// ------------------------------------------------------------------------------------------------
+// ARCHIVO DE CREACION DEL API
+// ------------------------------------------------------------------------------------------------
+// Importamos moongoose para administrar mongoDb
 var mongoose = require('mongoose');
-// instanciamos nuestro archivo de aplicacion
+// Importamos nuestro archivo de aplicacion denominado app
 var app = require('./app');
-// variable de puerto de conexion de nuestro api
+// Creamos una variable de puerto de conexion  en el puerto 3800
 var port = 3800;
-// iniciamos mongoose
+// Iniciamos mongooses
 mongoose.Promise = global.Promise;
-// conexion a la base de datos alojada en mongoDb atlas
-mongoose.connect('mongodb+srv://NefosAppUser:dana2002@cluster0-rxn0l.mongodb.net/test?retryWrites=true&w=majority'
+// Configuramos la onexion a la base de datos alojada en mongoDb atlas
+mongoose.connect('mongodb+srv://NefosAppUser:dana2002@cluster0-rxn0l.mongodb.net/test?retryWrites='
+    + 'true&w=majority'
     , { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
+        // si la conexcion de realizo con exito informamos por consola
         console.log("la conexion a la base de datos Api de NefosSocial se se realizo con exito");
-        // abrimos el puerto
+        // informamos en que puerto queda bierto para las coneciones 
         app.listen(port, () => {
             console.log("servidor corriendo http://localhost:3800");
         })
     })
+    // en caso de encontarrnos con un error informamos por consola
     .catch(err => console.log(err));
+// ------------------------------------------------------------------------------------------------
