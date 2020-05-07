f4efbf533baa866a6986e10ed57866c7f1f99232
Sebastian Echazú <sebastianechazuedu@gmail.com>
2020-05-07 12:14:52 -0300

    agrege comentarios en app y en jwt

--- api/app.js
+++ api/app.js
@@ -1,41 +1,47 @@
 'use strict'
-//-----------------------------------------------------------------------------------------
-// ARCHIVO DE CONFIGURACION DE EXPRESS
-//-----------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+// ARCHIVO DE CONFIGURACION DE LA APLICACION
+// ------------------------------------------------------------------------------------------------
+// importamos el modulo expreess
 var express = require('express');
+// importamos el modulo body-parser
 var bodyParser = require('body-parser');
+// creamos una instancia de exprees
 var app = express();
-//-----------------------------------------------------------------------------------------
-// INSTANCIACION DE LA RUTAS
-//-----------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+// RUTAS
+// ------------------------------------------------------------------------------------------------
+// creamos una variable por cada ruta que tenga la aplicacion
 var user_routes = require('./routes/user');
 var follow_routes = require('./routes/follow');
 var publication_routes = require('./routes/Publication');
 var message_routes = require('./routes/message');
-//-----------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 // MIDDLEWARES
-//-----------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+// instanciamos los midlaware 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//-----------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 // CORS
-//-----------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 // configurar cabeceras http
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With'
+        + ', Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
-//-----------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 // CREACION DE LAS RUTAS
-//-----------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+// anexamos a la variable app las rutas 
 app.use('/api', user_routes);
 app.use('/api', follow_routes);
 app.use('/api', publication_routes);
 app.use('/api', message_routes);
-//-----------------------------------------------------------------------------------------
-// EXPORTAMOS FUERA DEL ARCHIVO
-//-----------------------------------------------------------------------------------------
+// Exportamos la variable app
 module.exports = app;
+// ------------------------------------------------------------------------------------------------
--- api/services/jwt.js
+++ api/services/jwt.js
@@ -1,10 +1,16 @@
 'use strict'
-
+// ------------------------------------------------------------------------------------------------
+// SERVICIO PARA CREACION DE TOKENS
+// ------------------------------------------------------------------------------------------------
+// Importamos el modulo jwt-simple
 var jwt = require('jwt-simple');
+// importamos el modulo de moment para administrar fechas
 var moment = require('moment');
+// Creamos una variable para usar como clave secreta
 var secret = 'clave_secreta_NefosSocialApp';
-//crear token
-exports.createToken = function(user){
+// Exportamos una fucnion que crea tokens
+exports.createToken = function (user) {
+    // creamos que datos van a ser tokenizados
     var payload = {
         sub: user._id,
         name: user.name,
@@ -13,8 +19,12 @@ exports.createToken = function(user){
         email: user.email,
         role: user.role,
         image: user.image,
+        // parsemamos la fecha 
         iat: moment().unix(),
-        exp: moment().add(30,'days').unix()
+        // informamos la cantidad de dias de expiracion del token
+        exp: moment().add(30, 'days').unix()
     };
-    return jwt.encode(payload,secret);
+    // devolvemos la variable pay load con los datos encriptados
+    return jwt.encode(payload, secret);
 };
+// ------------------------------------------------------------------------------------------------
