9624d7c8a9bc3072cf85ee8b5334e829a9547121
Sebastian Echazú <sebastianechazuedu@gmail.com>
2020-05-07 11:54:57 -0300

    comentarios  en autentificacion

--- api/middlewares/authenticated.js
+++ api/middlewares/authenticated.js
@@ -1,16 +1,20 @@
 'use strict'
+// ------------------------------------------------------------------------------------------------
+// MIDDLEWARE DE AUTENTIFICACION
+// ------------------------------------------------------------------------------------------------
+// Importamos a jwt-simple
 var jwt = require('jwt-simple');
-
+// importamos moment para adminitrars fechas
 var moment = require('moment');
-
+// Creamos una variable secret para almacenar en nuestra app una clave
 var secret = 'clave_secreta_NefosSocialApp';
-
+// Exportamos fuera del modulo una funcion  
 exports.ensureAuth = function (req, res, next) {
-    //si el token no llega apropiadamente
+    // Manejos un respuesta de error 403 si el token no llega apropiadamente
     if (!req.headers.authorization) {
         return res.status(403).send({ message: 'la peticion no tiene la cabecera de autenticacion' });
     }
-    //asignamos el toquena una variable y eliminamos las comillas dobles o simples reemplazandolas por nada
+    //asignamos el toquen a una variable y eliminamos las comillas dobles o simples reemplazandolas por nada
     var token = req.headers.authorization.replace(/['"]+/g, '');
     try {
         //decodificamos el token
@@ -22,9 +26,8 @@ exports.ensureAuth = function (req, res, next) {
     } catch (error) {
         return res.status(404).send({ message: 'el token no es valido' });
     }
-
+    // si el token viene correctamente verificamos el usuario
     req.user = payload;
-
     next();
-
 }
+// ------------------------------------------------------------------------------------------------
