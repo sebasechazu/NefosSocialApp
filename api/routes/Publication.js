a35fd040abd3984cf07e7b371d6231e14a390370
Sebastian Echazu <sebastianechazuedu@gmail.com>
2020-05-10 11:17:57 -0300

    comentarios de rutas de publicaciones actualizados

--- api/routes/Publication.js
+++ api/routes/Publication.js
@@ -1,15 +1,22 @@
 'use strict'
-
+// ------------------------------------------------------------------------------------------------
+// IMPORTS
+// -----------------------------------------------------------------------------------------------
+// Importa expres para crear las rutas
 var express = require('express');
+// Importa el controlador de publicaciones
 var PublicationController = require('../controllers/publication');
+// Crea la variable api para administrar las rutas con express
 var api = express.Router();
+// Importa el middleware de autentificacion 
 var md_auth = require('..//middlewares/authenticated');
-
+// Importa multiparty para el manejo de archivos
 var multipart = require('connect-multiparty');
+// Crea la variable donde informa la ubicacion de la carpetas de archivos
 var md_upload = multipart({uploadDir:'./uploads/publications'});
-//-------------------------------------------------------------------------------------------------
-//RUTAS PARA EXPORTAR
-//-------------------------------------------------------------------------------------------------
+//  -------------------------------------------------------------------------------------------------
+// RUTAS 
+// -------------------------------------------------------------------------------------------------
 api.post('/publication',md_auth.ensureAuth,PublicationController.savePublication);
 api.get('/publications/:page?',md_auth.ensureAuth,PublicationController.getPublications);
 api.get('/publications-user/:user/:page?',md_auth.ensureAuth,PublicationController.getPublicationsUser);
--- api/routes/follow.js
+++ api/routes/follow.js
@@ -1,5 +1,5 @@
 'use strict'
-
+// -----------------------------
 var express = require('express');
 var FollowController = require('..//controllers/follow');
 var api = express.Router();
