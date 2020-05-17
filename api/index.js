99f517dc23e5c29ee09827b919bea04d3a21f095
Sebastian Echazú <sebastianechazuedu@gmail.com>
2020-05-17 11:05:17 -0300

    agrege archivo de variables globales

--- api/index.js
+++ api/index.js
@@ -7,16 +7,15 @@ var mongoose = require('mongoose');
 // Importamos nuestro archivo de aplicacion denominado app
 var app = require('./app');
 // Creamos una variable de puerto de conexion  en el puerto 3800
-var port = 3800;
+require('./global');
 // Iniciamos mongooses
 mongoose.Promise = global.Promise;
-// Configuramos la onexion a la base de datos alojada en mongoDb atlas
-mongoose.connect('mongodb+srv://NefosAppUser:dana2002@cluster0-rxn0l.mongodb.net/test?retryWrites='
-    + 'true&w=majority'
+// Configuramos la conexion a la base de datos alojada en mongoDb atlas
+mongoose.connect(mongoCloud
     , { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
+    .then(() => { mongoCloud
         // si la conexcion de realizo con exito informamos por consola
-        console.log("la conexion a la base de datos Api de NefosSocial se se realizo con exito");
+        console.log("la conexion a la base de datos  NefosSocial se se realizo con exito");
         // informamos en que puerto queda bierto para las coneciones 
         app.listen(port, () => {
             console.log("servidor corriendo http://localhost:3800");
