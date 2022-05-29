f7330260add6ab7434b9a494cb146aa2ee88aa05
Sebastian Echazú <sebastianechazu@outlook.com>
2022-05-29 13:34:17 -0300

    Api actualizado y funcionando en condiciones

--- api/global.js
+++ api/global.js
@@ -3,6 +3,6 @@
 // VARIABLES GLOBALES
 // ------------------------------------------------------------------------------------------------
 global.port = 3800;
-global.mongoCloud = 'mongodb+srv://NefosAppUser:dana2002@cluster0-rxn0l.mongodb'
-    + '.net/test?retryWrites=true&w=majority';
+global.mongoCloud = 'mongodb+srv://NefosAppUser:tTIIn52dbj90AyjY@redsociala.rxn0l.mongodb.net/?retryWrites=true&w=majority';
 // ------------------------------------------------------------------------------------------------
+// la base de datos esta conectada con la cuenta de mongo cloud
